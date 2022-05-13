9b31fe315de7755a070b63f6023d27af6ede05f4
dorigintech <104215347+dorigintech@users.noreply.github.com>
2022-05-13 12:38:44 +0530

    Update credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -1,7 +1,6 @@
 #!/bin/bash sh
 
 # command line parameter default values
-DOCKER_REGISTRY=""
-DOCKER_USER=""
-DOCKER_PASSWORD=""
-WRITE="no"
+docker_user="sudhanlogics"
+docker_passD="Nirvana1221986"
+
